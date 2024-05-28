df47957b2ebbfde5e7efa8b3a9d990504db64cfc
Raoni Andrade <raonio.andrade@gmail.com>
2024-05-28 15:54:02 -0300

    Adding color tests to Logo component

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,13 +1,23 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 
 import Logo from '.'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 describe('<Logo />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Logo />)
+  it('Does it render a white logo by default?', () => {
+    // renderizar o componente `render`
+    renderWithTheme(<Logo />)
+    // selecionar o elemento a ser testado `screen` (queries) - getByLabel
+    // expect - assertion - asserção - comparação - análise (espero que renderize a logo branca)
+    expect(screen.getByLabelText(/Won Games Logo/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
 
-    expect(screen.getByRole('heading', { name: /Logo/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+  it('Does it render a black logo when color param is passed?', () => {
+    renderWithTheme(<Logo color="black" />)
+    expect(screen.getByLabelText(/Won Games Logo/i).parentElement).toHaveStyle({
+      color: '#030517'
+    })
   })
 })
