f0baaf9d659db6e6b44eb89becca70f94a84b33f
Raoni Andrade <raonio.andrade@gmail.com>
2024-06-10 16:14:54 -0300

    Adding Button size tests, fullWidth options and tests

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -1,13 +1,43 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 
 import Button from '.'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 describe('<Button />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Button />)
+  it('Does it render the medium size button by default?', () => {
+    renderWithTheme(<Button>Buy now</Button>)
 
-    expect(screen.getByRole('heading', { name: /Button/i })).toBeInTheDocument()
+    expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyle({
+      height: '4rem',
+      padding: '0.8rem 3.2rem',
+      'font-size': '1.4rem'
+    })
+  })
 
-    expect(container.firstChild).toMatchSnapshot()
+  it('Does it render the small size button when params are passed?', () => {
+    renderWithTheme(<Button size="small">Buy now</Button>)
+
+    expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyle({
+      height: '3rem',
+      'font-size': '1.2rem'
+    })
+  })
+
+  it('Does it render the large size button when params are passed?', () => {
+    renderWithTheme(<Button size="large">Buy now</Button>)
+
+    expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyle({
+      height: '5rem',
+      padding: '0.8rem 4.8rem',
+      'font-size': '1.6rem'
+    })
+  })
+
+  it('Does it render the fullWidth button when params are passed?', () => {
+    renderWithTheme(<Button fullWidth>Buy now</Button>)
+
+    expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyle({
+      width: '100%'
+    })
   })
 })
