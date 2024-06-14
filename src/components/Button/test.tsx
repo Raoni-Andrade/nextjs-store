e5bb421b0a37f6c4014386ff95cff89cd9d08c32
Raoni Andrade <raonio.andrade@gmail.com>
2024-06-14 14:28:59 -0300

    Adding button icon and its tests

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -2,16 +2,19 @@ import { screen } from '@testing-library/react'
 
 import Button from '.'
 import { renderWithTheme } from 'utils/tests/helpers'
+import { AddShoppingCart } from 'styled-icons/material'
 
 describe('<Button />', () => {
   it('Does it render the medium size button by default?', () => {
-    renderWithTheme(<Button>Buy now</Button>)
+    const { container } = renderWithTheme(<Button>Buy now</Button>)
 
     expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyle({
       height: '4rem',
       padding: '0.8rem 3.2rem',
       'font-size': '1.4rem'
     })
+
+    expect(container.firstChild).toMatchSnapshot()
   })
 
   it('Does it render the small size button when params are passed?', () => {
@@ -39,5 +42,14 @@ describe('<Button />', () => {
     expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyle({
       width: '100%'
     })
+  })
+
+  it('Does it render the button with an icon?', () => {
+    renderWithTheme(
+      <Button icon={<AddShoppingCart data-testid="icon" />}>Buy now</Button>
+    )
+
+    expect(screen.getByText(/buy now/i)).toBeInTheDocument()
+    expect(screen.getByTestId('icon')).toBeInTheDocument()
   })
 })
