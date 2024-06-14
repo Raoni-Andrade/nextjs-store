2498621eaf1e5ab1a420af4bcde52ebbeea759b8
Raoni Andrade <raonio.andrade@gmail.com>
2024-06-14 15:46:17 -0300

    Adding open and close menu logic with TDD

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -1,4 +1,4 @@
-import { screen } from '@testing-library/react'
+import { fireEvent, screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import Menu from '.'
@@ -13,5 +13,26 @@ describe('<Menu />', () => {
     expect(screen.getByLabelText(/logo/i)).toBeVisible()
     expect(screen.getByRole('img', { name: /won games/i })).toBeVisible()
     expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument()
+  })
+
+  it('Does the menu open and close?', () => {
+    renderWithTheme(<Menu />)
+
+    // selecionar o menu
+    const fullMenuElement = screen.getByRole('navigation', { hidden: true })
+
+    // verificar se o menu está escondido
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+
+    // clicar no botão de abrir o menu e verificar se ele abriu
+    fireEvent.click(screen.getByLabelText(/open menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false')
+    expect(fullMenuElement).toHaveStyle({ opacity: 1 })
+
+    // clicar no botão de abrir o menu e verificar se ele fechou
+    fireEvent.click(screen.getByLabelText(/close menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
   })
 })
