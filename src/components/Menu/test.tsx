d10387a656cec92da38575663e0f95c617b24436
Raoni Andrade <raonio.andrade@gmail.com>
2024-06-14 17:05:19 -0300

    Adding Menu tests for logged in and out

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -35,4 +35,24 @@ describe('<Menu />', () => {
     expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
     expect(fullMenuElement).toHaveStyle({ opacity: 0 })
   })
+
+  it('Does it show registerBox when not logged in?', () => {
+    renderWithTheme(<Menu />)
+
+    fireEvent.click(screen.getByLabelText(/open menu/i))
+    expect(screen.getByText(/log in now/i)).toBeInTheDocument()
+    expect(screen.getByText(/sign up/i)).toBeInTheDocument()
+    expect(screen.queryByText(/my account/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/wishlist/i)).not.toBeInTheDocument()
+  })
+
+  it('Does it show my account details when logged in?', () => {
+    renderWithTheme(<Menu username="Fezco" />)
+
+    fireEvent.click(screen.getByLabelText(/open menu/i))
+    expect(screen.queryByText(/log in now/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/sign up/i)).not.toBeInTheDocument()
+    expect(screen.getByText(/my account/i)).toBeInTheDocument()
+    expect(screen.getByText(/wishlist/i)).toBeInTheDocument()
+  })
 })
