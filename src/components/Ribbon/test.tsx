76de5b9034a696ddaf6c26287730696eec45dc72
Raoni Andrade <raonio.andrade@gmail.com>
2024-06-24 15:40:50 -0300

    Creating Ribbon component and basic tests

---
+++ src/components/Ribbon/test.tsx
@@ -0,0 +1,48 @@
+import { screen } from '@testing-library/react'
+
+import Ribbon from '.'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+describe('<Ribbon />', () => {
+  it('Does it render the Ribbon text correctly?', () => {
+    const { debug, container } = renderWithTheme(<Ribbon>Best Seller</Ribbon>)
+
+    expect(screen.getByText(/best seller/i)).toBeInTheDocument()
+
+    debug(container)
+  })
+
+  it('Does it render the Ribbon with the primary color?', () => {
+    renderWithTheme(<Ribbon>Best Seller</Ribbon>)
+
+    expect(screen.getByText(/best seller/i)).toHaveStyle({
+      backgroundColor: '#F231A5'
+    })
+  })
+
+  it('Does it render the Ribbon with the secondary color?', () => {
+    renderWithTheme(<Ribbon color="secondary">Best Seller</Ribbon>)
+
+    expect(screen.getByText(/best seller/i)).toHaveStyle({
+      backgroundColor: '#3CD3C1'
+    })
+  })
+
+  it('Does it render the Ribbon with the normal size as default?', () => {
+    renderWithTheme(<Ribbon>Best Seller</Ribbon>)
+
+    expect(screen.getByText(/best seller/i)).toHaveStyle({
+      height: '3.6rem',
+      fontSize: '1.4rem'
+    })
+  })
+
+  it('Does it render the Ribbon with the small size?', () => {
+    renderWithTheme(<Ribbon size="small">Best Seller</Ribbon>)
+
+    expect(screen.getByText(/best seller/i)).toHaveStyle({
+      height: '2.6rem',
+      fontSize: '1.2rem'
+    })
+  })
+})
