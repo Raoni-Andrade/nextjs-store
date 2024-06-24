5daa36778eb135a13e562cfd773e38e12d0f9008
Raoni Andrade <raonio.andrade@gmail.com>
2024-06-24 17:11:12 -0300

    Updating Button tests

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -54,13 +54,11 @@ describe('<Button />', () => {
   })
 
   it('Does it render the button as a link?', () => {
-    const { debug, container } = renderWithTheme(
+    renderWithTheme(
       <Button as="a" href="/link">
         Buy now
       </Button>
     )
-
-    debug(container)
 
     expect(screen.getByRole('link', { name: /buy now/i })).toHaveAttribute(
       'href',
