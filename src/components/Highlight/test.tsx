3feb175fe4dbdd02c09268645327f75ec79779c5
Raoni Andrade <raonio.andrade@gmail.com>
2024-06-25 14:32:15 -0300

    Adding Highlight alignment for both right and left sides of floatImage, and tests

--- src/components/Highlight/test.tsx
+++ src/components/Highlight/test.tsx
@@ -2,6 +2,7 @@ import { screen } from '@testing-library/react'
 
 import Highlight from '.'
 import { renderWithTheme } from 'utils/tests/helpers'
+import * as S from './styles'
 
 const props = {
   title: 'Red Dead it’s back',
@@ -14,7 +15,7 @@ const props = {
 
 describe('<Highlight />', () => {
   it('Does it render the headings and button?', () => {
-    const { container } = renderWithTheme(<Highlight {...props} />)
+    renderWithTheme(<Highlight {...props} />)
 
     expect(
       screen.getByRole('heading', { name: /red dead it’s back/i })
@@ -42,5 +43,33 @@ describe('<Highlight />', () => {
       'src',
       '/img/red-dead-float.png'
     )
+  })
+
+  it('Does it render the float image on the right side by default?', () => {
+    const { container } = renderWithTheme(<Highlight {...props} />)
+
+    expect(container.firstChild).toHaveStyleRule(
+      'grid-template-areas',
+      "'floatimage content'"
+    )
+
+    expect(container.firstChild).toHaveStyleRule('text-align', 'right', {
+      modifier: `${S.Content}`
+    })
+  })
+
+  it('Does it render the float image on the left side when alignment is passed?', () => {
+    const { container } = renderWithTheme(
+      <Highlight {...props} alignment="left" />
+    )
+
+    expect(container.firstChild).toHaveStyleRule(
+      'grid-template-areas',
+      "'content floatimage'"
+    )
+
+    expect(container.firstChild).toHaveStyleRule('text-align', 'left', {
+      modifier: `${S.Content}`
+    })
   })
 })
