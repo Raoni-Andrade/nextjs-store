c5da1b379c022a83277f9769426d71306e52f982
Raoni Andrade <raonio.andrade@gmail.com>
2024-06-25 12:55:07 -0300

    Adding Highlight initial structure and basic tests with TDD

--- src/components/Highlight/test.tsx
+++ src/components/Highlight/test.tsx
@@ -1,13 +1,27 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 
 import Highlight from '.'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+const props = {
+  title: 'Red Dead it’s back',
+  subtitle: 'Come see John’s new adventures',
+  buttonLabel: 'Buy now',
+  buttonLink: '/rdr2'
+}
 
 describe('<Highlight />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Highlight />)
+  it('Does it render the headings and button?', () => {
+    const { container } = renderWithTheme(<Highlight {...props} />)
 
-    expect(screen.getByRole('heading', { name: /Highlight/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /red dead it’s back/i })
+    ).toBeInTheDocument()
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(
+      screen.getByRole('heading', { name: /come see john’s new adventures/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('link', { name: /buy now/i })).toBeInTheDocument()
   })
 })
