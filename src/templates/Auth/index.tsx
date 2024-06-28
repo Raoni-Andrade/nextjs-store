bcc78624542fb680e1df977e929ed845016feac8
Raoni Andrade <raonio.andrade@gmail.com>
2024-06-28 19:10:47 -0300

    Initializing Desktop style for Auth template

--- src/templates/Auth/index.tsx
+++ src/templates/Auth/index.tsx
@@ -10,23 +10,28 @@ type AuthProps = {
 const Auth = ({ title, children }: AuthProps) => (
   <S.Wrapper>
     <S.BannerBlock>
-      <Logo />
+      <S.BannerContent>
+        <Logo />
+        <div>
+          <Heading>All your favorite games in one place</Heading>
+          <S.Subtitle>
+            <strong>WON</strong> is the best and most complete gaming platform.
+          </S.Subtitle>
+        </div>
 
-      <Heading>All your favorite games in one place</Heading>
-      <S.Subtitle>
-        <strong>WON</strong> is the best and most complete gaming platform.
-      </S.Subtitle>
-
-      <S.Footer>Raoni Andrade 2024 © All rights reserved.</S.Footer>
+        <S.Footer>Raoni Andrade 2024 © All rights reserved.</S.Footer>
+      </S.BannerContent>
     </S.BannerBlock>
 
     <S.Content>
-      <Logo color="black" size="large" />
-      <Heading color="black" lineColor="secondary" lineLeft>
-        {title}
-      </Heading>
+      <S.ContentWrapper>
+        <Logo color="black" size="large" />
+        <Heading color="black" lineColor="secondary" lineLeft>
+          {title}
+        </Heading>
 
-      {children}
+        {children}
+      </S.ContentWrapper>
     </S.Content>
   </S.Wrapper>
 )
